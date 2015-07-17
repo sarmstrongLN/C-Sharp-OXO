3fb528d8cca27836a4c7f804e7251c2f66d7bfd8
Stewart Armstrong <stewart.armstrong@hotmail.co.uk>
2015-07-17 22:11:38 +0100

    Tests and implementation for Tile class

--- OXO/src/Tile.cs
+++ OXO/src/Tile.cs
@@ -2,7 +2,18 @@
 
 public class Tile : ITile
 {
+    char m_cDisplayValue;
+
 	public Tile()
 	{
+        m_cDisplayValue = ' ';
 	}
+
+    public void setValue(char val) {
+        m_cDisplayValue = val;
+    }
+
+    public char getValue() {
+        return m_cDisplayValue;
+    }
 }
---
+++ OXO/test/OXO-Tests/TileTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OXO_Tests
+{
+    [TestClass]
+    public class TileTests
+    {
+        Tile testSubject;
+
+        public TileTests()
+        {
+            testSubject = new Tile();    
+        }
+
+        [TestMethod]
+        public void TestDefaultValueIsASpace()
+        {
+            Assert.AreEqual(' ', testSubject.getValue());
+        }
+
+        [TestMethod]
+        public void TestValueIsAnOAfterSet()
+        {
+            testSubject.setValue('O');
+            Assert.AreEqual('O', testSubject.getValue());
+        }
+    }
+}
